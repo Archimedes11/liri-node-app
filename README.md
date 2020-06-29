b94617efe00d06ab29acc5370f6628b9739b9ee2
Adam Wolff <adamkwolff@gmail.com>
2020-06-29 16:36:28 -0500

    Typed out the heart of the program and took pictures, working on the read me.

--- README.md
+++ README.md
@@ -1,1 +1,96 @@
-# liri-node-app
+# LIRI-BOT
+
+LIRI-NODE-APP
+
+LIRI is like iPhone's SIRI.  However, while SIRI is a Speech Interpretation and Recognition Interface, LIRI is a _language_Interpretation and Recognition Interface.  LIRI will be a command line node app that takes in parameters and gives you back data.  LIRI will search Spotify for songs, Bands in Town for concerts, and OMDB for movies.
+
+OVERVIEW: 
+
+In this project we focused on using Node.js in the command line and taking parameters (process.argv)to call a function and display output to the console.
+
+When running Node with following strings, Node takes the strings as an array, with spaces as the separator.
+    The first item in the array is the Node.exe path
+    Second item is the current directory
+The LIRI app takes the third item of the array for the command, while the rest are used as arguments for API calls.
+
+SOFTWARE USED:
+
+NODE.JS - a JavaScript runtime built on Chrome's V8 JavaScript engine.
+Axios
+    Installation: node install axios
+    Used for grabbing data from:
+    OMDB API
+    Bands In Town API
+Node-Spotify-API
+    Installation: npm install node-spotify-api
+Spotify module
+    "This is a universal wrapper/client for the Spotify Web API that runs on Node.js and the browser, using browserify/webpack/rollup.
+Moment
+    Moment.js module is used for formatting time.
+    Installation: npm install moment
+Dotenv
+    A zero-dependency module for loading environment variables from .env into process.env.
+    Installation: npm install dotenv
+FS module
+    "The fs module provides an API for interacting with the file system in a manner closely modeled around standard POSIX functions." (link)
+    Installation: npm install fs
+    var fs = require("fs")
+Request
+    "The request module is for making HTTP requests, it is really a wrapper around Node's built in http module."
+    
+Require module
+    "This is a builtin for Node, require function is the easiest way to include modules that exist in separate files.  Functionality of require is that it reads a Javascript file, executes the file, and then proceeds to return the exports object.
+    Installation: npm install require
+
+Inquirer
+    "Inquirer eases the process of asking end user questions, parsing, validating answers, managing hierarchical prompts and providing error feedback.
+Bands In Town -Events module
+    "This is a wrapper to use Bands in Town api, returns a random song/album/artist.
+
+The "getMyInfo" object
+
+The getMyInfo object contains multiple methods for the LIRI app to determine which command was entered and then calls that specific method to obtain and display specific information.
+    processArgs method
+        gets an array from [process.argv](https://nodejs.org/api/process.html#process_process_argv), which is a "property that returns an array containing the command line arguments passed when the Node.js process was launched."
+    checkCommand method
+        Uses the switch/case statement checks if the command argument matches an existing method and calls that method.
+    concertThis method.
+    spotifyThis method.
+    movieThis method.
+    doWhatItSays method.
+    addToFile method.
+
+LIRI is run in the command line.
+
+    node liri.js concert-this <artist/band name here> runs the concertThis method along with the name of the artist or band as an argument. It gets info from the Bands In Town API using Axios and displays multiple show events.
+
+        If no argument (Artist or Band) is entered, it will search using "George Strait" as a default value
+        Save command and argument to addToFile method.
+        Screenshot:
+        
+        <img src="liri-node-app/assets/Concert-This.png"><img>
+
+    node liri.js movie-this <movie name here> runs the movieThis method using Axios and OMDB API.
+
+        If a movie is entered, it returns the movie title, year, IMDB Rating, RT rating, country, language, plot, and actors.
+        If no movie is entered, a default value of "Mr Nobody" is used.
+        Save command and argument to addToFile method.
+        Screenshot:
+
+    node liri.js do-what-it-says runs the doWhatItSays method using the FS module to get a string from the random.txt file.
+
+        The "," in the string will be used as a delimiter using `.split(",") and saves the string as two items in an array.
+        The first item is used as the command and the second item is the query argument.
+        The command and query argument are passed to the checkCommand method which then calls the appropriate method.
+        Save command and argument to addToFile method.
+        Screenshot:
+
+    The addToFile method uses the FS module to append a string to the log.txt file.
+
+        Moment.js is used to getthe current time and format it as [/DD/YYYY].
+        Here's the heading of each appended string:
+        [MM/DD/YYY] <command-name> <argument>
+        Screenshot
+
+    Log.txt file:
+        log.txt
--- liri.js
+++ liri.js
@@ -4,4 +4,194 @@ var axios = require("axios"); // Make XMLHttpRequests from the browser
 var moment = require("moment"); // time formatter
 var keys = require("./keys.js"); // imports keys.js files
 var Spotify = require('node-spotify-api'); //spotify api npm
+
 var spotify = new Spotify(keys.spotify); //access to spotify keys
+
+var nodeCommand;
+var nodeArgString;
+
+var hRule = "--------------------------------------------------\n"; //formatting
+
+var getMyInfo = {
+    // get arguments from command line node liri.js <arguments>
+    processArgs: function (arg) {
+        var tempArray = [];
+        nodeCommand = arg[2].toLowerCase();
+        for (var i = 3; i < arg.length; i++) {
+            tempArray.push(arg[i]);
+        }
+        nodeArgString = tempArray.join(" ");
+        // console.log("nodeArgString:", nodeArgString);
+        this.checkCommand(nodeCommand, nodeArgString);
+    },
+
+    // determine command
+    checkCommand: function (nodeCmd, nodeArg) {
+        switch (nodeCmd) {
+            case "concert-this":
+                this.concertThis(nodeArg);
+                break;
+            case "spotify-this-song":
+                this.spotifyThis(nodeArg);
+                break;
+            case "movie-this":
+                this.movieThis(nodeArg);
+                break;
+            case "do-what-it-says":
+                this.doWhatItSays(nodeArg);
+                break;
+            default:
+                console.log("Sorry, I don't know that command.");
+                break;
+        }
+    },
+
+    // concert-this uses the Bands In Town Artist Events API
+    // format: node liri.js concert-this <artist/band name here>
+    concertThis: function (query) {
+        if (query === '') {
+            query = 'George Strait';
+        }
+        axios.get("https://rest.bandsintown.com/artists/" + query + "/events?app_id=codingbootcamp&display-limit=5").then(
+            function (response) {
+                
+                var concertInfo = "";
+                for (var i = 0; i < response.data.length; i++) {
+                    // display JSON response
+                    concertInfo += "Venue: " + response.data[i].venue.name + "\n";
+                    concertInfo += "Location: " + response.data[i].venue.city + ", ";
+                    if (response.data[i].venue.region) {
+                        concertInfo += response.data[i].venue.region + ", ";
+                    }
+                    concertInfo += response.data[i].venue.country + "\n";
+                    var date = moment(response.data[i].datetime, moment.ISO_8601).format("MM/DD/YYYY");
+                    concertInfo += "Date of Event: " + date + "\n\n";
+                    console.log(concertInfo);
+                }
+
+                // add to log.txt
+                addToFile(nodeCommand, query, concertInfo);
+            })
+            .catch(function (error) {
+                console.log(`Sorry, the band "${query}" could not be found.`);
+            });
+
+    },
+
+    // spotify-this-song
+    // format: node liri.js spotify-this-song '<song name here>'
+    spotifyThis: function (query) {
+        if (query === '') {
+            query = 'The Chair';
+        }
+        spotify.search({ type: 'track', query: query, limit: 5 }, function (err, data) {
+            if (err) {
+                return console.log(`Sorry, "${query}" could not be found.`);
+            }
+            
+            var songInfo = "";
+            for (var i = 0; i < data.tracks.items.length; i++) {
+                var artistsArray = [];
+                // Artist or Artists logic
+                if (data.tracks.items[i].artists.length > 1) {
+                    songInfo += "Artists: "
+                } else {
+                    songInfo += "Artist: "
+                }
+
+                // concat multiple artists array
+                for (var j = 0; j < data.tracks.items[i].artists.length; j++) {
+                    artistsArray.push(data.tracks.items[i].artists[j].name);
+                }
+                songInfo += artistsArray.join(", ") + "\n";
+                songInfo += "Song Name: " + data.tracks.items[i].name + "\n";
+                songInfo += "Preview Link: " + data.tracks.items[i].preview_url + "\n";
+                songInfo += "Album: " + data.tracks.items[i].album.name + "\n\n";
+
+
+                console.log(songInfo);
+            }
+
+            addToFile(nodeCommand, query, songInfo);
+
+        });
+    },
+
+    // movie-this - use axios to retrieve data from OMDB API
+    // format: node liri.js movie-this '<movie name here>'
+    movieThis: function (query) {
+        if (query === '') {
+            query = 'Mr Nobody';
+        }
+        axios.get("http://www.omdbapi.com/?t=" + query + "&y=&plot=short&apikey=trilogy")
+            // success
+            .then(function (response) {
+                // display JSON response
+                var movieInfo = "Title: " + response.data.Title + "\n";
+                movieInfo += "Year: " + response.data.Year + "\n";
+                movieInfo += "IMDB Rating: " + response.data.Ratings[0].Value + "\n";
+                movieInfo += "Rotten Tomatoes Rating: " + response.data.Ratings[1].Value + "\n";
+                movieInfo += "Country: " + response.data.Country + "\n";
+                movieInfo += "Language: " + response.data.Language + "\n";
+                movieInfo += "Plot: " + response.data.Plot + "\n";
+                movieInfo += "Actors: " + response.data.Actors + "\n";
+                // PUSH TO FILE
+
+                addToFile(nodeCommand, query, movieInfo);
+                console.log(movieInfo);
+                // DATE, COMMAND, PARAMETER - LOG should be short, not have everything.
+                // LOGS are concise.
+            })
+            // catch any errors
+            .catch(function(error) {
+                // console.log("!!!ERROR!!!:", error);
+                console.log(`Sorry, the movie "${query}" was not found in the OMBD`);
+            })
+
+    },
+
+    // do-what-it-says
+    // format: node liri.js do-what-it-says
+    doWhatItSays: function (query) {
+        // FS read method
+        fs.readFile("random.txt", "utf8", function (error, data) {
+
+            // If the code experiences any errors it will log the error to the console.
+            if (error) {
+                return console.log(error);
+            }
+
+            // check data from file
+            console.log(data);
+
+            // split into array
+            var dataArray = data.split(",");
+
+            // call the checkCommand method
+            getMyInfo.checkCommand(dataArray[0], dataArray[1]);
+
+        });
+    }
+}
+
+// BONUS In addition to logging the data to your terminal/bash 
+// window, output the data to a .txt file called log.txt.
+// Make sure you append each command you run to the log.txt file.
+
+// APPEND searches to log.txt
+function addToFile(nodeCmd, query, info) {
+    var logFile = hRule + "[" + moment().format("MM/DD/YYYY") + "] ";
+    logFile += nodeCmd + " " + query + "\n" + hRule + info;
+
+    fs.appendFile("log.txt", logFile + "\n", function (err) {
+        if (err) {
+            return console.log(err);
+        }
+        else {
+            console.log("log.txt file was appended!");
+        }
+    });
+};
+
+
+getMyInfo.processArgs(process.argv);
